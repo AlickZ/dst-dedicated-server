c929df94a622c7a109263d17ca1ff1232b101466
AlickZ <563396148@qq.com>
2021-01-04 22:53:33 +0800

    更新 start-container-server.sh

--- start-container-server.sh
+++ start-container-server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Check for game updates before each start. If the game client updates and your server is out of date, you won't be
-# able to see it on the server list. If that happens just restart the containers and you should get the latest version
+# 每次开始前检查游戏更新。如果游戏客户端更新了，而你的服务器已经过时了，你就不会被更新
+# 可以在服务器列表中看到它。如果发生这种情况，只需重新启动容器，您应该得到最新版本
 /home/dst/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
 
-# Copy dedicated_server_mods_setup.lua
+# 复制mod配置文件
 ds_mods_setup="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
 if [ -f "$ds_mods_setup" ]
 then
   cp $ds_mods_setup "$HOME/server_dst/mods/"
 fi
 
-# Copy modoverrides.lua
+# 复制 modoverrides.lua
 modoverrides="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
 if [ -f "$modoverrides" ]
 then
